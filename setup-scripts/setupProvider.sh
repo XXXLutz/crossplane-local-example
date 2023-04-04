fc3edbc9fd2c3777043ee36151bc3f51a5b920f8
KasnocknDave <dhn@xxxlgroup.com>
2023-04-04 16:34:04 +0200

    add readme

--- setup-scripts/setupProvider.sh
+++ setup-scripts/setupProvider.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-export GH_TOKEN=$(cat ../secret/token)
-export GH_OWNER=$(cat ../secret/user)
-
 kubectl apply -f ../providers/github/install.yaml
-envsubst <  ../providers/github/provider-secret.yaml | kubectl apply -f -
+kubectl apply -f ../providers/github/provider-secret.yaml
 kubectl apply -f ../providers/github/provider-config.yaml
