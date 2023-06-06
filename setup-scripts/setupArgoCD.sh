72cddbb9951b4562b3fa60aa7415283208ac32bf
KasnocknDave <dhn@xxxlgroup.com>
2023-06-06 16:30:34 +0200

    remove space

--- setup-scripts/setupArgoCD.sh
+++ setup-scripts/setupArgoCD.sh
@@ -2,5 +2,5 @@
 
 kubectl create namespace argocd
 kubectl apply -k ../deploy/argocd
-kubectl  wait --for condition=established --timeout=60s crd/applications.argoproj.io --timeout=60s
+kubectl wait --for condition=established --timeout=60s crd/applications.argoproj.io --timeout=60s
 kubectl apply -k ../deploy/argocd-apps
